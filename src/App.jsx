3fb201ff49098ad98bfe4c73daf3b6d991a807ee
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-04-13 02:51:36 -0500

    added the searchbar component and wrote the logic for the search term to only search for s string

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CardList from './components/CardList';
+import SearchBar from './components/SearchBar'
 import {robots} from './robots'
 function App() {
   return (
     <div className='tc pa4' >
+      <SearchBar />
       <CardList robots={robots} />
     </div>
   );
